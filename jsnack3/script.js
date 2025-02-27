2bb8195a03b4c8d6c7cb7d94a4a94e44cfdfa180
willymariino <wg.mariino@gmail.com>
2025-02-27 12:35:18 +0100

    jsnack3

---
+++ jsnack3/script.js
@@ -0,0 +1,4 @@
+// il software deve chiedere 10 volte all'utente di inserire un numero
+// il programma stampa la somma di tutti i numeri inseriti
+for (let i = 0; i < 10; i++)
+    parseInt(prompt('inserisci un numero'));
