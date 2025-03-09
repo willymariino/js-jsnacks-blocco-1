fd6fecdbae8263de3a54209c6b6e5bbf0627441a
willymariino <wg.mariino@gmail.com>
2025-03-09 18:39:46 +0100

    finished snack 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,9 +2,17 @@
 // il programma stampa la somma di tutti i numeri inseriti
 
 function askUserNumbers() {
-    let result = []
-    for (i = 0; i >= 10; i++)
-        parseInt(prompt("inserisci un numero"))
+    let somma = 0
+    for (i = 0; i < 10; i++) {
+        let numero = parseInt(prompt("inserisci un numero"))
+        somma += numero;
 
+
+    }
+    console.log("la somma dei numeri è" + somma)
+    // se l'obiettivo è stampare in console, non serve il return
 
 }
+
+askUserNumbers()
+
