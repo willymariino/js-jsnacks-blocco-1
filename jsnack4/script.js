0a865b4bd5e17b7d2d2ee133fecdf58310e80c4f
willymariino <wg.mariino@gmail.com>
2025-03-10 09:34:43 +0100

    fixed snack 4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,10 +1,10 @@
 //crea un array vuoto, chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'arrays
 function getOddNumbers() {
     let oddNumbers = []
-    for (let i = 0; i <= 6; i++) {
+    for (let i = 0; i < 6; i++) {
         let numero = parseInt(prompt("inserisci un numero"))
         if (numero % 2 !== 0) {
-            oddNumbers.push(numero[i])
+            oddNumbers.push(numero)
         }
     }
 
